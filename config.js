e4ce657d79ae8428a417c4da8eee28505ac4429a
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-05-09 15:58:04 +0100

    Fixed package.json

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {
-    "uploadDirectory": "/tmp/uploads/"
+    "uploadDirectory": "/home/qgrootbl/public_html/uploads/"
+  , "publicDirectory": "http://ceres.tchpc.tcd.ie/~qgrootbl/uploads/"
   , "fedoraURL": "howest-server.tchpc.tcd.ie"
   , "fedoraPort": 9191
   , "fedoraAuth": "fedoraAdmin:admin"
