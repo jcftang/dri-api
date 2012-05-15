d05e909bded45b3952a19e19d685dc2a159c0e03
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-05-15 11:10:23 +0100

    Updated config template file

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    "uploadDirectory": "/home/qgrootbl/public_html/uploads/"
-  , "publicDirectory": "http://ceres.tchpc.tcd.ie/~qgrootbl/uploads/"
-  , "fedoraURL": "howest-server.tchpc.tcd.ie"
-  , "fedoraPort": 9191
-  , "fedoraAuth": "fedoraAdmin:admin"
-}
-module.exports = config
