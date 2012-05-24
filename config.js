a7143efd4842cef43bd6925228530275e6a0e06d
matthias van wambeke <matthias.van.wambeke@howest.be>
2012-05-24 12:10:09 +0100

    changed upload to xmlhttpupload

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 var config = {
-    "uploadDirectory": "/tmp/uploads/" /*Local directory to store media files*/
-  , "publicDirectory": "http://strife.tchpc.tcd.ie/~mvanwamb/uploads/" /*public URL to the directory where media files are stored*/
-  , "fedoraURL": "howest-server.tchpc.tcd.ie" /*URL to the Fedora instance*/
+    "uploadDirectory": __dirname + "/tmp/uploads/" /*Local directory to store media files, __dirname gives the location of this file*/
+  , "publicDirectory": "http://localhost:4000/uploads/" /*public URL to the directory where media files are stored*/
+  , "fedoraURL": "localhost" /*URL to the Fedora instance*/
   , "fedoraPort": 9191 /*Port on which Fedora runs*/
-  , "fedoraAuth": "fedoraAdmin:admin" /*Authentification info for Fedora*/
-  , "fedoraNamespace": "8FedoraLib" /*Namespace to be used for the Fedora objects*/
+  , "fedoraAuth": "username:password" /*Authentification info for Fedora*/
+  , "fedoraNamespace": "test" /*Namespace to be used for the Fedora objects*/
+  , "mongoDBURL": "mongodb://localhost/dri4" /*URL to the mongoDB instance*/
 }
 module.exports = config
--- routes/upload.js
+++ routes/upload.js
@@ -11,11 +11,13 @@ exports.index = function(req, res) {
 		'content-type' : 'text/html'
 	});
 	res.end('Post images to this url');
+	
 }
 // Creates an object with the given data
 
 var amountOfFiles = 1;
 exports.create = function(req, res) {
+	console.log(req.files)
 	if(req.files.upload) {
 		uploadFile(res, req, req.files.upload, 1)
 	} else if(req.files.files[0].size != undefined) {
@@ -28,17 +30,21 @@ exports.create = function(req, res) {
 
 }
 function uploadFile(res, req, file, count) {
+	console.log("test")
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers','*')
+	res.setHeader('Access-Control-Allow-Methods', ['POST', 'GET', 'OPTIONS'])
 	dri.uploadFile(file, function(result) {
 		//console.log(count-1)
 		if(req.files.upload) {
 			res.send(result);
 		} else if(amountOfFiles == 1) {
+			console.log("1")
 			req.files.files[0].fileLocation = result;
-			res.redirect(req.body.redirect + "[" + JSON.stringify(req.files.files[0]) + "]")
+			res.send("[" + JSON.stringify(req.files.files[0]) + "]")
 		} else if(count == amountOfFiles && amountOfFiles > 1) {
 			req.files.files[0][count-1].fileLocation = result;
-			res.redirect(req.body.redirect + JSON.stringify(req.files.files[0]))
+			res.send(JSON.stringify(req.files.files[0]))
 		}else if(amountOfFiles > 1){
 			req.files.files[0][count-1].fileLocation = result;
 		}
